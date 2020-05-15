1eeccfe4ab16b7f1abfe21e5c823b08835514956
James Simpson <james@cellbloc.com>
2020-05-14 20:00:59 -0400

    James StateManager

---
+++ Assets/Scripts/StateManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateManager : MonoBehaviour
+{
+    public enum StateView { Default, GetStateStory, GetStateStoryArea, GetNextState, GetResponses};
+
+    public StateView _CurrentState = StateView.Default;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            _CurrentState = StateView.Default;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            _CurrentState = StateView.GetStateStory;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            _CurrentState = StateView.GetStateStoryArea;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            _CurrentState = StateView.GetNextState;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            _CurrentState = StateView.GetResponses;
+        }
+    }
+}
